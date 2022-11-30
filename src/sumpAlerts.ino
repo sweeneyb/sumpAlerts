ee3445f57f498086f254e0a68e3b5e73ebf075cf
sweeneyb <tab126@gmail.com>
2022-11-30 12:51:12 -0500

    reduced false positives

--- src/sumpAlerts.ino
+++ src/sumpAlerts.ino
@@ -1,38 +1,66 @@
 bool isNotificationPending = false;
-SystemSleepConfiguration config;
+bool isRunning = false;
+bool sendFlakeAlert = false;
+SystemSleepConfiguration loopSleep;
+SystemSleepConfiguration debounceSleep;
+
 
 void setup() {
   pinMode(D5, INPUT_PULLUP);
 
   Particle.publish("status", getPumpStatusString("Startup: "));
-  config.mode(SystemSleepMode::STOP)
+  isRunning = isPumpRunning();
+  loopSleep.mode(SystemSleepMode::STOP)
       .duration(500ms);
+  debounceSleep.mode(SystemSleepMode::STOP)
+      .duration(100ms);      
   
   attachInterrupt(D5, pumpStatusChanged, CHANGE);
 }
 
 
 void pumpStatusChanged() {
-  isNotificationPending = true;
+  if (isRunning != isPumpRunning() ) {
+    isNotificationPending = true;
+  } else {
+    sendFlakeAlert = true;
+  }
+  isRunning = isPumpRunning();
+
 }
 
 void loop() {
   String prefix = "Pump status change: ";
-  SystemSleepResult result = System.sleep(config);
+  SystemSleepResult result = System.sleep(loopSleep);
+
+  if(sendFlakeAlert) {
+    Particle.publish("status", "flake alert");
+    sendFlakeAlert = false;
+  }
 
   if(isNotificationPending) {
     Particle.publish("status", getPumpStatusString(prefix));
-    Particle.publish("pushbullet", getPumpStatusString(prefix));
+    if (isRunning) {
+      Particle.publish("status", "Interrupt running: true");
+    } else {
+      Particle.publish("status", "Interrupt running: false");
+    }
+    bool running = isPumpRunning();
+    SystemSleepResult result = System.sleep(debounceSleep);
+    if(running == isPumpRunning() ) {
+      
+      Particle.publish("pushbullet", getPumpStatusString(prefix));
+    }
     isNotificationPending = false;
   }
 }
 
-bool getPumpStatus() {
+bool isPumpRunning() {
   return !digitalRead(D5);
 }
 
 String getPumpStatusString(String prefix) {
-  bool pin = getPumpStatus();
+  bool pin = isPumpRunning();
   String status = prefix + "  Running: ";
   if(pin) {
     status += "true";
